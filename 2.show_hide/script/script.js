222fe2d31b42cb9ac01ab9a988fe511d75c69a81
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-04 22:24:17 +0600

    jQuery show hide task complete

---
+++ 2.show_hide/script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.hide-btn').click(function () {
+        $('.show-hide').hide();
+    });
+});
+
+$(document).ready(function () {
+    $('.show-btn').click(function () {
+        $('.show-hide').show();
+    });
+});
+
