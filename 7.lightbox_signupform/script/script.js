1257a9cd49db3ae16c9bdd62ecc3710a5135384c
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-07 11:56:17 +0600

    creating lightbox using jQuery

---
+++ 7.lightbox_signupform/script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // $('.callback-btn').click(function () {
+    //     $('.green').css('background', 'yellow').slideUp(2000).slideDown(2000, function () {
+    //         alert('After slide up and down callback function alert method working.')
+    //     });
+    // });
+
+    // Fade in lightbox after half a second
+    $(".lightbox").delay(500).fadeIn(1000);
+});  
