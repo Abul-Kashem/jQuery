acbf6cb25fcecf30540ae285325e5ad69f20f8a4
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-08 20:51:55 +0600

    changing attribute properties

---
+++ 14.changing_attribute_properties/js/script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function () {
+
+  var link = $('#link');
+  console.log(link.attr('href'));
+  console.log(link.attr('title'));
+
+  link.attr('href', "http://www.w3school.com");
+
+
+  var radioButton = $("input[type=radio]:first");
+  console.log(radioButton.prop("checked"));
+  console.log(radioButton.attr("checked"));
+
+  $("input[type=text]").val("Abul Kashem");
+ $("input[type=email]").val("kashem@gmail.com");
+  $("input[type=range]").val(9);
+
+});
+
+
