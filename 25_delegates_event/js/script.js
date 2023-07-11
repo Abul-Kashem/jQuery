402798d646e89402db4e988cb171fdb0c96a51ba
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-11 22:42:02 +0600

    delegate event using jQuery

---
+++ 25_delegates_event/js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+
+  $('p').click(function () {
+    $(this).slideUp();
+  });
+
+
+  $('ul li li').click(function () {
+    $(this).slideUp();
+  });
+
+
+});
+
+
