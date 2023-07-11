89ce3a62e33f921e20ea9690a05bd50eaf1be7b6
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-11 21:25:26 +0600

    hover event using jQuery

---
+++ 21_hover_event/js/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+
+  $('#click-event-fire').hover(function (e) {
+    console.log(e);
+    alert('Hover event fire!')
+  })
+
+
+  $('.red-box').hover(function () {
+    var current_opacity = $(this).css('opacity');
+    $(this).fadeTo(500, current_opacity - .3);
+  });
+
+
+
+});
+
+
